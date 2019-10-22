7a14b76ede8e562c40c4cacdb36688a627da37cb
Scotty Smith <chssmith@roanoke.edu>
2019-10-22 16:16:19 -0400

    Fixed medical view

--- resources/views/medical.blade.php
+++ resources/views/medical.blade.php
@@ -4,6 +4,28 @@
 	Medical Information
 @endsection
 
+@section("stylesheets")
+	@parent
+	<link type="text/css" rel="stylesheet" href="{{ asset("css/global.css") }}" />
+	<style>
+		textarea{
+			margin-bottom:10px;
+			width: 50%;
+			height: 200px;
+		}
+
+		label{
+			font-size:20px;
+			width: 100%;
+		}
+		@media(max-width: 767px) {
+			textarea {
+				width: 100%;
+			}
+		}
+	</style>
+@endsection
+
 @section('javascript')
 	<script>
 	function hideShowLocal(show){
@@ -22,50 +44,50 @@
 @endsection
 
 @section("content")
-	<form 	id = "MedicalInfo" method = "POST"
-     	  	action = "{{ action("StudentInformationController@medicalInfoUpdate") }}">
+	<form id="MedicalInfo" method="POST"
+		action="{{ action("StudentInformationController@medicalInfoUpdate") }}">
 		{{ csrf_field() }}
 
-		<div class = "row">
-   	     	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-   	     		<h4> List any serious health concerns: Check all that apply. </h4>
-	   	     		@foreach($health_concerns as $concerns) 
-	   	     			<div>
-	   	     				<div class="pretty p-default">  	     			
-			            		<input type = "checkbox" name = "concerns[]" value = "{{$concerns->id}}" id = "{{$concerns->id}}" @if(!empty($concerns->student_concerns)) checked @endif />
-			            		<div class="state p-primary">
-				            		<label> {{$concerns->description}}</label>
-				            	</div>
-		   	     			</div>
-	   	     			</div>
-	   	     		@endforeach
+		<div class="row">
+			<div class="col-xs-12">
+				<h4> List any serious health concerns: Check all that apply. </h4>
+				@foreach($health_concerns as $concerns)
+					<div>
+						<div class="pretty p-default">
+							<input type="checkbox" name="concerns[]" value="{{$concerns->id}}" id="{{$concerns->id}}" @if(!empty($concerns->student_concerns)) checked @endif />
+							<div class="state p-primary">
+								<label> {{$concerns->description}}</label>
+							</div>
+						</div>
+					</div>
+				@endforeach
 
-   	     		<div>
-     				<div class="pretty p-default">  	 
-		        		<input type = "checkbox" name = "other" value = "other" id = "other"
+				<div>
+					<div class="pretty p-default">
+						<input type="checkbox" name="other" value="other" id="other"
 		    			@if(!empty($other_concern)) checked @endif />
-		    			<div class="state p-primary">
-			    			<label>Other</label>
-			    		</div>
+	    			<div class="state p-primary">
+		    			<label>Other</label>
 		    		</div>
-		    	</div>
-   	     	</div>
-   	    </div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-   	    <div class="row" id="other_span" @if(empty($other_concern)) style="display:none" @endif>   	    	
-   	     	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-   	     		<h4> Please list any other health concerns </h4>
-   	     		<textarea name="other_concerns" rows="5" cols="50">@if(!empty($other_concern)){{$other_concern->other_concern}}@endif</textarea>
-   	     	</div>
-   	    </div>
+		<div class="row" id="other_span" @if(empty($other_concern)) style="display:none" @endif>
+			<div class="col-xs-12">
+				<label for="other_concerns"> Please list any other health concerns </label>
+				<textarea name="other_concerns" id="other_concerns">@if(!empty($other_concern)){{$other_concern->other_concern}}@endif</textarea>
+			</div>
+		</div>
 
-   	    <div class = "row">
-        	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        		<div class="btn-toolbar">
-	    		    <button type = "submit" class = "btn btn-lg btn-success pull-right"> Save and Continue </button>
-	        		<a href="{{action('StudentInformationController@index')}}" class="btn btn-lg btn-danger pull-right"> Cancel </a>
-        		</div>
-        	</div>
-        </div>
-    </form>
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="btn-toolbar">
+					<button type="submit" class="btn btn-lg btn-success pull-right"> Save and Continue </button>
+					<a href="{{action('StudentInformationController@index')}}" class="btn btn-lg btn-danger pull-right"> Cancel </a>
+				</div>
+			</div>
+		</div>
+	</form>
 @endsection
