b1219ec109a0ba5f5bf26deffdea26b2cc7ba28c
Scotty Smith <chssmith@roanoke.edu>
2020-06-02 16:04:41 -0400

    Fixing excel downloads

--- app/Exports/RegistrationExport.php
+++ app/Exports/RegistrationExport.php
@@ -10,7 +10,7 @@ class RegistrationExport implements FromView
 {
     public function view(): View
     {
-      $all_registrations = Registrations::with(["session_dates", "student", "student_info", "guests", "mode_of_travel"])->whereNotNull("fkey_mode_of_travel_id")->get();
+      $all_registrations = Registrations::whereHas("student")->with(["session_dates", "student", "student_info", "guests", "mode_of_travel"])->whereNotNull("fkey_mode_of_travel_id")->get();
       $max_guests = \DB::select(\DB::raw("SELECT MAX(guests) AS max_guests FROM (SELECT count(id) AS guests FROM orientation.guest_info WHERE deleted_at IS NULL GROUP BY fkey_registration_id) sub_query"));
       $max_guests = $max_guests[0]->max_guests;
 
